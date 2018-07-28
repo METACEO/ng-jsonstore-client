c2b782086531eaf746f962eaf2b1508c2bd0f9dc
James Allen <james.allen@meta.ceo>
2018-07-27 22:13:32 -0500

    Introduce the @angular/material module.

---
+++ src/app/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+const modules = [
+  MatToolbarModule,
+];
+
+@NgModule({
+  imports: modules,
+  exports: modules,
+})
+export class MaterialModule {
+}
