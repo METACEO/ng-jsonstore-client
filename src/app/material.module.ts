036c9cfc4e87dd5a26f28c0e28997717f03fe1ad
James Allen <james.allen@meta.ceo>
2018-07-27 22:31:14 -0500

    Adding a toolbar; centering and wrapping the content with a card component.

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 const modules = [
+  MatButtonModule,
+  MatCardModule,
   MatToolbarModule,
 ];
 
